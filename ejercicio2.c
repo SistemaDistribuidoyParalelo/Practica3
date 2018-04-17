a10d101c15fa24450ac09b1b6d3188c45f79cf6a
aCastelli95 <castelli_alanunlp@hotmail.com>
2018-04-16 22:00:41 -0300

    Terminado el 2 pero sin poder compilar

--- ejercicio2.c
+++ ejercicio2.c
@@ -13,14 +13,24 @@ int main(int argc,char*argv[]){
  omp_set_num_threads(numThreads);
  scale=2.78;
  x=0.0;
-
+/*
+	sqrt(interno) = raiz cuadrada de lo interno
+	El ejercicio esta realizando una suma y la esta guardando en los valores de X. Lo que sucede es que las cuentas 
+	se iran superponiendo o se iran guardando mal dado que se pueden pisar los resultados de los hilos.
+	Es seccion critica, cada uno deberia de entrar de a uno, hacer la cuenta, guardar el resultado y seguir.
+*/
  #pragma omp parallel for
  for(i=1;i<=N;i++){
-	x= x + sqrt(i*scale) + 2*x;
+	#pragma omp critical(sumaX)
+		x = x + sqrt(i*scale) + 2*x;
  }
 
  printf("\n Resultado: %f \n",x);
 
  return(0);
 }
+
+
+
+
 
